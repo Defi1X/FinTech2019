7c60895a27276e64995ceafa07eff981a237eef6
Leb Nickley <egorcraft@gmail.com>
2019-03-15 23:53:36 +0700

    Update main.sol

--- main.sol
+++ main.sol
@@ -6,7 +6,7 @@ contract Main {
         minter = msg.sender;
     }
     uint public QOC = 0;
-    
+    uint private PRICE = 30;
     event print(string text, uint value);
     
     mapping (address => uint) public balances; 
@@ -14,20 +14,8 @@ contract Main {
     mapping (uint    => address) public Owners;
     mapping (uint    => uint) public sellBoard;
     mapping (uint    => uint) public sellBoardPrices;
-    
-    
-    
-    
-    function send(address To, uint amount) private {
-        if(balances[msg.sender] < amount) return;
-        balances[msg.sender]    -= amount;
-        balances[To]            += amount;
-    }
-    
-    function gift(address To, uint amount) private{
-       // msg.value 
-        balances[To] += amount;
-    }
+
+
     
     function BuyByID(uint number) payable public{
         require(msg.value >= sellBoardPrices[number]);
@@ -38,24 +26,25 @@ contract Main {
         Owners[number] = msg.sender;
     }
     
+    //## NOT WORKING
     function SellByID(uint number) public{
         require(msg.sender == Owners[number]);
         var seller = msg.sender;
         Owners[number] = minter;
-        seller.transfer(30);
+        seller.transfer(PRICE);
         
     }
 
     
     function Fight(uint player1, uint player2) public returns (uint){
-        var St1 = Characters[player1][1];
-        var Ag1 = Characters[player1][2];
-        var In1 = Characters[player1][3];
+        var St1    = Characters[player1][1];
+        var Ag1    = Characters[player1][2];
+        var In1    = Characters[player1][3];
         var Power1 = (stringToUint(St1) + stringToUint(Ag1)) * stringToUint(In1);
         
-        var St2 = Characters[player2][1];
-        var Ag2 = Characters[player2][2];
-        var In2 = Characters[player2][3];
+        var St2    = Characters[player2][1];
+        var Ag2    = Characters[player2][2];
+        var In2    = Characters[player2][3];
         var Power2 = (stringToUint(St2) + stringToUint(Ag2)) * stringToUint(In2);
         
         if(Power1 >  Power2) return 1;
@@ -64,12 +53,9 @@ contract Main {
     }
     
     function BuyNewCharacter() payable public{
-        require(msg.value >= 30);
+        require(msg.value >= PRICE);
         uint payed = msg.value;
-       
-        /*if(payed < 30){
-            minter.transfer(payed); // warning here
-        } */
+        
         minter.transfer(payed);
         gen();
     }
@@ -83,16 +69,21 @@ contract Main {
         
         QOC += 1;
         
-        
         Characters[QOC - 1][0] = Name;
         Characters[QOC - 1][1] = St;
         Characters[QOC - 1][2] = Ag;
         Characters[QOC - 1][3] = In;
         Owners[QOC - 1] = Owner;
-        
-        
-        
-        
+    }
+    
+    function renameCharacter(uint id, string newName) public{
+        require(msg.sender == Owners[id]);
+        Characters[id][0] = newName;
+    }
+    
+    function giftCharacter(uint id,address ToUser){
+        require(msg.sender == Owners[id]);
+        Owners[id] = ToUser;
     }
     
     function GetOwner(uint some) public returns (address) {
@@ -103,6 +94,14 @@ contract Main {
         var rand = uint(sha3(now)) % 10;
         if(rand == 0) rand += 1;
         return uint2str(rand); 
+    }
+    
+    
+    // ### Only Minter function!
+    // By this function minter can change price of new character
+    function ChangePrice(uint newPrice) public {
+        require(msg.sender == minter);
+        PRICE = newPrice;
     }
     
     // StackOverflow functions
